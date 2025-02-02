be08d3951cb0d2801b26aa3fa14464dd2df8aec3
Nikhil Mekle <158294493+nikhilmekle@users.noreply.github.com>
2025-02-02 13:39:52 +0530

    first commit

--- src/component/Join/Join.jsx
+++ src/component/Join/Join.jsx
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react'
-import './Join.css'
-import emailjs from '@emailjs/browser'
+import React, { useRef } from "react";
+import "./Join.css";
+import emailjs from "@emailjs/browser";
 export default function Join() {
   const form = useRef();
 
@@ -8,39 +8,47 @@ export default function Join() {
     e.preventDefault();
 
     emailjs
-      .sendForm('service_4h52mrr', 'template_dkiwtvu', form.current, 'J77SXF1V383ltuwVf'
+      .sendForm(
+        "service_t2xnwbh",
+        "template_dkiwtvu",
+        form.current,
+        "7HMHyoqpvDcVRlpX1"
       )
       .then(
         () => {
-          console.log('SUCCESS!');
+          console.log("SUCCESS!");
         },
         (error) => {
-          console.log('FAILED...', error.text);
-        },
+          console.log("FAILED...", error.text);
+        }
       );
-    }
+  };
 
-    return (
-        <div className="Join" id="join-us">
-            <div className="left-j">
-                <hr />
-                <div>
-                    <span className='stroke-text'>READY TO</span>
-                    <span>LEVEL UP</span>
-                </div>
-                <div>
-                    <span>YOUR BODY</span>
-                    <span className='stroke-text'>WITH US ? </span>
-                </div>
-            </div>
-            <div className="right-j">
-            <form ref={form} onSubmit={sendEmail}  className='email-container'>
-              
-                    <input type="email" name='user_email' placeholder= 'Enter Your Email' />
-                    <button className='btn btn-j'type="submit">Join Now</button>
-                </form>
-            </div>
-
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
         </div>
-    )
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text">WITH US ? </span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form ref={form} onSubmit={sendEmail} className="email-container">
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Enter Your Email"
+          />
+          <button className="btn btn-j" type="submit">
+            Join Now
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
